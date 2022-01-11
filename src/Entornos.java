0ca4e97a2ff7025b1065b20d884943dbb5400a31
Pablo Conca <pabloconcasanchez@gmail.com>
2022-01-11 19:58:41 +0100

    Mostrar menu

---
+++ src/Entornos.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Entornos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduir nous elements a la carta (Opció 1)");
+        System.out.println("Llistar els elements que es troben a la carta ( Opció 2)");
+        System.out.println("Modificar un element que es troba a la carta ( Opció 3)");
+    }
+}
